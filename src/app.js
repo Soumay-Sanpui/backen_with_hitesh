e217f9c8c49fb0e5312ecb4d07fea6e59bd0c9dc
somu sanpui <somusanpui@somus-Mac.local>
2024-02-01 21:09:52 +0530

    added api response handler

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// getting data in form of json wiht limit upto 16kb.
+app.use(express.json({limit: "16kb"}))
+// configuring express to get data from url as well and extented option is for the getting objects inside objects.
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export {app}
