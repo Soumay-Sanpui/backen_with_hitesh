ae40d7c10f3a381fca56e76f9694ec64ae527f5a
somu sanpui <somusanpui@somus-Mac.local>
2024-02-02 22:01:07 +0530

    configured multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs";
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        // upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded no delete the file from
+        console.log("file uploaded");
+        return response;
+    } catch (error) {
+        // remove the locally saved file.
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
+export {uploadOnCloudinary}
