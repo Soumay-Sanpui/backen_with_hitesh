78e029a56e6a420df43d42cfb6a876c16746361f
somu sanpui <somusanpui@somus-Mac.local>
2024-02-03 10:56:57 +0530

    configured user route

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js"
-
+import dotenv from "dotenv"
+dotenv.config({path: "../../.env"})
 const connectDB = async () => {
     try {
-        const connectionInstance =  await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        const connectionInstance =  await mongoose.connect(`mongodb+srv://somu:somu@cluster0.1f4iwcz.mongodb.net/${DB_NAME}`)
         console.log(`DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
+        console.log(process.env.MONGO_URL)
         console.log(error);
         process.exit(1);
     }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv"
 
 dotenv.config({path: './env'})
 connectDB().then(()=> {
-    app.listen(process.env.PORT || 8000,()=> {
-        console.log(`Server is running at: http://localhost:${process.env.PORT}`)
+    app.listen(8000,()=> {
+        console.log(`Server is running at: http://localhost:8000`)
     })
 
     app.on("error",(error) => {
