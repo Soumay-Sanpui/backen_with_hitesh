1292b785cfbf8b87d12f5a61892e41ba86b1b727
Soumay-Sanpui <somusanpui9@gmail.com>
2024-02-04 11:16:26 +0530

    implemented user registration

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 
 dotenv.config({path: './env'})
 connectDB().then(()=> {
-    app.listen(8000,()=> {
+    app.listen(process.env.PORT || 8000,()=> {
         console.log(`Server is running at: http://localhost:8000`)
     })
 
